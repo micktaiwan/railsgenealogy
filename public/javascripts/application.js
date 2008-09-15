b5895dcf743ce5333970d8dcf2ab50e46213e042
faivrem <faivrem@b7716520-829a-11dd-9205-735403a6da14>
2008-09-15 22:54:57 +0000

    work in progress

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,32 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function new_person(id) {
-  new Draggable(id);
+
+
+// Class Graph
+// manage all the persons on the browser canvas 
+function Graph() {
+
+}
+ 
+
+// Class Person
+// inputs: DB id of the person 
+function Person(id, text) {
+  this.id = id
+  this.element = document.createElement('div');
+  this.element.innerHTML = text;
+  $('edit').appendChild(this.element);
+  this.element.setAttribute('id','person_'+id);
+  this.element.setAttribute('class','person');
+  new Draggable(this.element.id);
+
+  // add all family members in the graphic
+  // inputs: DB id of the person
+  this.add_family = function() {
+    //this.element.innerHTML = 'not done yet';
+    }
+  
   }
+
+
