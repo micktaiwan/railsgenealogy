5cb13eff665bf8388d838cc595eb9b57eb74a863
faivrem <faivrem@b7716520-829a-11dd-9205-735403a6da14>
2008-09-15 19:55:42 +0000

    people edition

--- trunk/app/controllers/people_controller.rb
+++ trunk/app/controllers/people_controller.rb
@@ -5,9 +5,19 @@ class PeopleController < ApplicationController
   end
   
   def create
-    @person = Person.new(params['person'])  
-    @person.save
-    render(:partial=>'person')
+    @person_list = Person.new(params['person'])  
+    @person_list.save
+    render(:partial=>'person_list')
+  end
+  
+  def edit
+    @person = Person.find(params['id'])
+  end
+  
+  def save
+    @person_list = Person.find(params['id'])  
+    @person_list.attributes = params['person']
+    @person_list.save
   end
   
 end
