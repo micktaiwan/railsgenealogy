60b7e029b185ad18b0b5e68889b759aab8243893
faivrem <faivrem@b7716520-829a-11dd-9205-735403a6da14>
2008-09-17 21:53:12 +0000

    search people for relations

--- trunk/app/controllers/people_controller.rb
+++ trunk/app/controllers/people_controller.rb
@@ -1,5 +1,7 @@
 class PeopleController < ApplicationController
 
+  protect_from_forgery :except => [:auto_complete_for_search_name] 
+
   def get_new_form
     render(:partial=>'new_form', :locals=>{:role=>params['role'], :id=>params['id']})
   end
@@ -9,12 +11,7 @@ class PeopleController < ApplicationController
     @person_list.save
     role = params[:role] 
     if role != nil
-      @for = Person.find(params['for_id'].to_i)
-      case role
-      when 'father': @for.father_id = @person_list.id
-      when 'mother': @for.mother_id = @person_list.id
-      end
-      @for.save
+      add_relation(@person_list.id,role,Person.find(params['for_id'].to_i))
     end
     
     render(:partial=>'person_list')
@@ -39,5 +36,34 @@ class PeopleController < ApplicationController
     render(:partial=>'add_parent_form', :locals=>{:role=>params['role']})
   end
   
+  def auto_complete_for_search_name
+    #re = Regexp.new("#{params[:person][:name]}", "i")
+    str = params[:search][:name]
+    @persons = Person.find(:all,:conditions=>"surname like '%#{str}%' or fam_name like '%#{str}%'").map {|p| p.surname+ " "+p.fam_name}
+    render :inline => "<%= content_tag(:ul, @persons.map { |p| content_tag(:li, h(p)) }) %>"
+  end
+
+  def add_rel
+    name =  params[:search][:name].split(' ')
+    @person = Person.find_by_surname_and_fam_name(name[0],name[1])
+    if(@person == nil)
+      render(:text=>'Pas trouvé '+params[:search][:name]+', mais en même c\'est pas éttonant avec ma fonction de merde', :status=>500)
+      return
+    end
+    add_relation(@person.id,params[:role],Person.find(params['for_id'].to_i))
+    render(:text=>'Ajouté, mais il faut tout rafraichir, je sais c\'est chiant...')
+  end
+  
+  # id of the new person
+  # role of this person: 'father', 'mother'
+  # for_who: a person object
+  def add_relation(id,role,for_who)
+    case role
+    when 'father': for_who.father_id = id
+    when 'mother': for_who.mother_id = id
+    end
+    for_who.save
+  end
+
 end
 
